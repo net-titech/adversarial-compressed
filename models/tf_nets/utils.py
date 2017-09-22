96ac7ec9195027afe560a36d2b5421cb06934c77
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2017-09-22 18:52:36 +0900

    copy convert data format from tensorflow source

--- models/tf_nets/utils.py
+++ models/tf_nets/utils.py
@@ -10,3 +10,27 @@ def log_training(step, bloss):
     else:
         elapsed = time.time() - start
         print("|\t{0:5.2f}(sec)| Step {1}: batch loss: {2}".format(elapsed, step, bloss))
+
+def convert_data_format(data_format, ndim):
+    """Copy from tensorflow/tensorflow/python/layers/utils.py"""
+    if data_format == "channels_last":
+        if ndim == 3:
+            return "NWC"
+        elif ndim == 4:
+            return "NHWC"
+        elif ndim == 5:
+            return "NDHWC"
+        else:
+            raise ValueError("Input rank not supported:", ndim)
+    elif data_format == "channels_first":
+        if ndim == 3:
+            return "NCW"
+        elif ndim == 4:
+            return "NCHW"
+        elif ndim == 5:
+            return "NCDHW"
+        else:
+            raise ValueError("Input rank not supported:", ndim)
+    else:
+        raise ValueError("Invalid data_format:", data_format)
+
