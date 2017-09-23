c4f57ac07a3f0ac5b198cadf9aab4f6df371669b
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2017-09-23 13:39:41 +0900

    train alexnet with l2 regularizers

--- models/tf_nets/train.py
+++ models/tf_nets/train.py
@@ -1,5 +1,6 @@
 from sys import argv
-from alexnet import AlexNet, AlexNetVD
+from alexnet import AlexNet
+from alexnet_vd import AlexNetVD
 import imagenet as im
 
 # TODO: Refactoring traing code
@@ -10,5 +11,8 @@ data_generator = im.gen_data
 #simple_alex = AlexNet(batch_size, init_lr=0.1)
 #simple_alex.train(data_generator, im.train_size, epoch=200, step_save=10000)
 
-vd_fc_alex = AlexNetVD(init_alpha=0.999, batch_size=batch_size, init_lr=0.1)
-vd_fc_alex.train(data_generator, im.train_size, epoch=30, step_save=10001)
+#vd_fc_alex = AlexNetVD(init_alpha=0.999, batch_size=batch_size, init_lr=0.1)
+#vd_fc_alex.train(data_generator, im.train_size, epoch=30, step_save=10001)
+
+l2_alex = AlexNet(batch_size, init_lr=0.01, name="AlexNet_L2")
+l2_alex.train(data_generator, im.train_size, epoch=200, step_save=10000)
