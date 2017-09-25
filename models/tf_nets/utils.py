1026a8f62a9bd5dc4d7df3f51dbc0e3c3ce67ee1
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2017-09-25 16:13:28 +0900

    move load weights to utils, remove net_utils

--- models/tf_nets/utils.py
+++ models/tf_nets/utils.py
@@ -1,4 +1,5 @@
 import time
+import tensorflow as tf
 
 start = 0
 
@@ -34,3 +35,15 @@ def convert_data_format(data_format, ndim):
     else:
         raise ValueError("Invalid data_format:", data_format)
 
+def load_weights(ckpt_file, net_class):
+    net = net_class()
+    config = tf.ConfigProto(device_count={"GPU":0})
+    with tf.Session(config=config) as sess:
+        net._build()
+        sess.run(tf.global_variables_initializer())
+        saver = tf.train.Saver()
+        saver.restore(sess, ckpt_file)
+        all_vars = {}
+        for var in tf.global_variables():
+            all_vars[var.name] = sess.run(var)
+    return all_vars 
