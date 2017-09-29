88d75d8e4a379ceac6e657d12c241d8aa16815cd
Hoang NT <hoangnt@ai.cs.titech.ac.jp>
2017-09-29 10:56:41 +0900

    training var dropout with smaller regularization scale

--- models/tf_nets/train.py
+++ models/tf_nets/train.py
@@ -17,10 +17,14 @@ data_generator = im.gen_data
 #l2_alex = AlexNet(batch_size, init_lr=0.05, name="AlexNet_L2")
 #l2_alex.train(data_generator, im.train_size, epoch=200, step_save=10000)
 
-# batch_size=512
-vd_fc_alex = AlexNetVD(init_alpha=1e-8, batch_size=batch_size*2, 
-                       regularization_weight=1.0,
-                       init_lr=0.0001, name="AlexNet_VDFC")
+# batch_size=512 - 20170928 - failed
+#vd_fc_alex = AlexNetVD(init_alpha=1e-8, batch_size=batch_size*2, 
+#                       regularization_weight=1.0,
+#                       init_lr=0.0001, name="AlexNet_VDFC")
+
+vd_fc_alex = AlexNetVD(init_alpha=1e-2, batch_size=batch_size,
+                       regularization_weight=0.1,
+                       init_lr=0.01, name="AlexNet_VDFC_REG_SCALED")
 try:
     vd_fc_alex.train(data_generator, im.train_size, epoch=30, step_save=10000)
 except KeyboardInterrupt:
